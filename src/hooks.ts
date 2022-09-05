b87502551eebf1b9c0417282f43427bfde0ebfdd
Alexander Galichenko <alexandr.galichenko@gmail.com>
2022-09-05 09:41:59 +0300

    added screenshotHooks (#23)
    Co-authored-by: Oleksandr_Halichenko <oleksandr_halichenko@epam.com>

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,8 @@
-import { After, Before } from '@cucumber/cucumber';
+import { After, AfterStep, Before, BeforeStep, Status } from '@cucumber/cucumber';
 import defaultTimeouts from './defaultTimeouts';
 import { Browser, remote } from 'webdriverio';
 import { po } from '@qavajs/po';
+import { ScreenshotEvent } from './screenshotEvent';
 
 declare global {
     var browser: Browser<'async'>;
@@ -19,6 +20,29 @@ Before(async function () {
     global.driver = global.browser;
     po.init(browser, { timeout: driverConfig.timeout.present });
     po.register(config.pageObject);
+});
+
+BeforeStep(async function () {
+    if (config.screenshot === ScreenshotEvent.BEFORE_STEP) {
+        try {
+            this.attach(await browser.takeScreenshot(), 'image/png');
+        } catch (err) {
+            console.warn(err)
+        }
+    }
+});
+
+AfterStep(async function (step) {
+    try {
+        if (
+            (config.screenshot === ScreenshotEvent.ON_FAIL && step.result.status === Status.FAILED) ||
+            config.screenshot === ScreenshotEvent.AFTER_STEP
+        ) {
+            this.attach(await browser.takeScreenshot(), 'image/png');
+        }
+    } catch (err) {
+        console.warn(err)
+    }
 });
 
 After(async function () {
---
+++ src/screenshotEvent.ts
@@ -0,0 +1,5 @@
+export enum ScreenshotEvent {
+    ON_FAIL = 'onFail',
+    BEFORE_STEP = 'beforeStep',
+    AFTER_STEP = 'afterStep'
+}
