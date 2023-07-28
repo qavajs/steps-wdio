f3811708a0e11779c5ae740ef99a2fbcc37a07aa
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-07-28 11:04:24 +0300

    added experimental snapshot attach (#99)
    * added experimental snapshot attach

--- src/events.ts
+++ src/events.ts
@@ -3,3 +3,9 @@ export enum ScreenshotEvent {
     BEFORE_STEP = 'beforeStep',
     AFTER_STEP = 'afterStep'
 }
+
+export enum SnapshotEvent {
+    ON_FAIL = 'onFail',
+    BEFORE_STEP = 'beforeStep',
+    AFTER_STEP = 'afterStep'
+}
