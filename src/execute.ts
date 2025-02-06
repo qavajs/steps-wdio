9adac83b43c47cde7307843e42f1c8522a4191ec
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-02-06 13:54:47 +0200

    improved readability of execute steps

--- src/execute.ts
+++ src/execute.ts
@@ -8,7 +8,7 @@ import { Locator } from './pageObject';
  * @example I execute '$fn' function // fn is function reference
  * @example I execute 'window.scrollBy(0, 100)' function
  */
-When('I execute {value} function', async function (functionKey: MemoryValue) {
+When('I execute {value} function/script', async function (functionKey: MemoryValue) {
     await this.wdio.browser.execute(await functionKey.value());
 });
 
@@ -19,7 +19,7 @@ When('I execute {value} function', async function (functionKey: MemoryValue) {
  * @example I execute '$fn' function and save result as 'result' // fn is function reference
  * @example I execute 'window.scrollY' function and save result as 'scroll'
  */
-When('I execute {value} function and save result as {value}', async function (functionKey: MemoryValue, memoryKey: MemoryValue) {
+When('I execute {value} function/script and save result as {value}', async function (functionKey: MemoryValue, memoryKey: MemoryValue) {
     memoryKey.set(await this.wdio.browser.execute(await functionKey.value()));
 });
 
@@ -30,7 +30,7 @@ When('I execute {value} function and save result as {value}', async function (fu
  * @example I execute '$fn' function on 'Component > Element' // fn is function reference
  * @example I execute 'arguments[0].scrollIntoView()' function on 'Component > Element'
  */
-When('I execute {value} function on {wdioLocator}', async function (functionKey: MemoryValue, locator: Locator) {
+When('I execute {value} function/script on {wdioLocator}', async function (functionKey: MemoryValue, locator: Locator) {
     const fn = await functionKey.value();
     const element = locator();
     await this.wdio.browser.execute(fn, await element.getElement());
@@ -44,7 +44,7 @@ When('I execute {value} function on {wdioLocator}', async function (functionKey:
  * @example I execute 'arguments[0].innerText' function on 'Component > Element' and save result as 'innerText'
  */
 When(
-    'I execute {value} function on {wdioLocator} and save result as {value}',
+    'I execute {value} function/script on {wdioLocator} and save result as {value}',
     async function (functionKey: MemoryValue, locator: Locator, memoryKey: MemoryValue) {
         const fn = await functionKey.value();
         const element = locator();
