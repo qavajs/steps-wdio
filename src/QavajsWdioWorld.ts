0d79970212d76737c448260069ab8c440728c108
Alex G <noemail>
2026-03-29 17:04:43 +0300

    improve readme

--- src/QavajsWdioWorld.ts
+++ src/QavajsWdioWorld.ts
@@ -3,6 +3,8 @@ import type { Locator } from './pageObject';
 import { Wdio } from './wdio';
 
 export interface QavajsWdioWorld extends IQavajsWorld {
+    config: any;
+    log: (message: string) => void;
     wdio: Wdio;
     element(path: string): Locator;
 }
